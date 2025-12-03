71df9d7a4de511763281b9a53258a8416cc47003
hsharame <hsharame@student.42.fr>
2025-12-03 19:23:55 +0100

    ex06 done

---
+++ Starting/ex06/filterstring.py
@@ -0,0 +1,39 @@
+import sys
+from ft_filter import ft_filter
+
+
+def main():
+    """
+    Program accepts two arguments: a string (S) and an integer (N).
+    The program output a list of words from S that have a length greater than N
+    """
+    try:
+        if len(sys.argv) == 3:
+            s = sys.argv[1]
+            try:
+                n = int(sys.argv[2])
+            except ValueError:
+                raise AssertionError("the arguments are bad")
+            words = s.split()
+            filtered = ft_filter(lambda w: greater_than_n(w, n), words)
+            result = [w for w in filtered]
+            print(result)
+        else:
+            raise AssertionError("the arguments are bad")
+    except AssertionError as error:
+        print(f"AssertionError: {error}")
+
+
+def greater_than_n(s: str, number: int):
+    """
+    Return True if length of S is greater than NUMBER.
+     Otherwise, return False.
+    """
+    if len(s) > number:
+        return True
+    else:
+        return False
+
+
+if __name__ == "__main__":
+    main()
--- Starting/ex06/ft_filter.py
+++ Starting/ex06/ft_filter.py
@@ -1,1 +1,26 @@
-help(filter)
+from typing import Iterable, Optional
+
+
+def ft_filter(function: Optional, iterable: Iterable) -> filter:
+    """
+    Return an iterator yielding those items of iterable for which function
+    is true. If function is None, return the items that are true.
+    """
+    for element in iterable:
+        if function is None:
+            if element:
+                yield element
+        else:
+            if function(element):
+                yield element
+
+# ages = [5, 12, 17, 18, 24, 32]
+# def myFunc(x):
+#   if x < 18:
+#     return False
+#   else:
+#     return True
+# print(list(ft_filter(myFunc, ages)))
+
+# items = [0, 1, "", "Hello", None]
+# print(list(ft_filter(None, items)))
