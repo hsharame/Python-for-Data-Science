ee7ac35ade01a6e4c74756fb4abe358e58ced28e
Hanna Sharameta Veulay <167891494+hsharame@users.noreply.github.com>
2026-01-27 15:42:47 +0000

    array(ex00): added errors handler

--- Array/ex00/give_bmi.py
+++ Array/ex00/give_bmi.py
@@ -3,6 +3,13 @@ def give_bmi(height: list[int | float], weight: list[int | float]) -> list[int |
     Takes 2 lists of integers or floats as input and returns a list
     of BMI values.
     """
+    if len(height) != len(weight):
+        raise ValueError("Lists must be of the same length")
+    for w, h in zip(weight, height):
+        if not isinstance(w, (int, float)) or not isinstance(h, (int, float)):
+            raise TypeError("Lists must contain only integers or floats")
+        if w <= 0 or h <= 0:
+            raise ValueError("Height and weight must be greater than 0")
     res = [w / h ** 2 for w, h in zip(weight, height)]
     return res
 
@@ -12,5 +19,10 @@ def apply_limit(bmi: list[int | float], limit: int) -> list[bool]:
     Accepts a list of integers or floats and an integer representing
     a limit as parameters. It returns a list of booleans (True if above the limit).
     """
+    if not isinstance(limit, int):
+        raise TypeError("Limit must be an integer")
+    for x in bmi:
+        if not isinstance(x, (int, float)):
+            raise TypeError("List must contain only integers or floats")
     res = [x >= limit for x in bmi]
     return res
--- Array/ex00/tester.py
+++ Array/ex00/tester.py
@@ -6,6 +6,42 @@ bmi = give_bmi(height, weight)
 print(bmi, type(bmi))
 print(apply_limit(bmi, 26))
 
-#  python tester.py
-# [22.507863455018317, 29.0359168241966] <class 'list'>
-# [False, True]
+print("*" * 50)
+try: 
+    height = [2.71, height]
+    weight = [165.3, 38.4]
+    bmi = give_bmi(height, weight)
+    print(bmi, type(bmi))
+    print(apply_limit(bmi, 26))
+except Exception as error:
+    print(error)
+
+print("*" * 50)
+try:
+    height = [2.71, 1.15]
+    weight = [165.3, 38.4, 158]
+    bmi = give_bmi(height, weight)
+    print(bmi, type(bmi))
+    print(apply_limit(bmi, 26))
+except Exception as error:
+    print(error)
+
+print("*" * 50)
+try:
+    height = [2.71, 1.15]
+    weight = [165.3, -38.4]
+    bmi = give_bmi(height, weight)
+    print(bmi, type(bmi))
+    print(apply_limit(bmi, 26))
+except Exception as error:
+    print(error)
+
+print("*" * 50)
+try:
+    height = [2.71, 1.15]
+    weight = [165.3, 38.4]
+    bmi = give_bmi(height, weight)
+    print(bmi, type(bmi))
+    print(apply_limit(26, bmi))
+except Exception as error:
+    print(error)
