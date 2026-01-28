5586e10c5c4fc96e5e73b9e676c0410dfb1f66c1
Hanna Sharameta Veulay <167891494+hsharame@users.noreply.github.com>
2026-01-28 16:00:17 +0000

    array(ex01): done

--- Array/ex01/array2D.py
+++ Array/ex01/array2D.py
@@ -5,9 +5,19 @@ def slice_me(family: list, start: int, end: int) -> list:
     Takes as parameters a 2D array, prints its shape, and returns a
     truncated version of the array based on the provided start and end arguments.
     """
+    if (
+        not isinstance(family, list)
+        or not isinstance(start, int)
+        or not isinstance(end, int)
+    ):
+        raise TypeError("Args must be: list, int, int")
+    if not all(isinstance(row, list) for row in family):
+        raise TypeError("A 2D list expected")
+    row_len = len(family[0])
+    if not all(len(row) == row_len for row in family):
+        raise TypeError("Lists are not the same size")
     arr = np.array(family)
     print("My shape is :", arr.shape)
     res = arr[start:end]
     print("My new shape is :", res.shape)
     return res.tolist()
-
--- Array/ex01/tester.py
+++ Array/ex01/tester.py
@@ -7,3 +7,35 @@ family = [[1.80, 78.4],
 print(slice_me(family, 0, 2))
 print(slice_me(family, 1, -2))
 
+print("*" * 50)
+try: 
+    family = [[1.80, 78.4], 6]
+    print(slice_me(family, 0, 2))
+    print(slice_me(family, 1, -2))
+except Exception as error:
+    print(error)
+
+print("*" * 50)
+try: 
+    family = [[1.80, 78.4], [2.15, 12.7], [2.15, 102.7, 67,9]]
+    print(slice_me(family, 0, 2))
+    print(slice_me(family, 1, -2))
+except Exception as error:
+    print(error)
+
+print("*" * 50)
+try: 
+    family = ("Hello", "toto!")
+    print(slice_me(family, 0, 2))
+    print(slice_me(family, 1, -2))
+except Exception as error:
+    print(error)
+
+print("*" * 50)
+try: 
+    family = [["Hello", "tata!"],
+    {"Hello" : "titi!"}]
+    print(slice_me(family, 0, 2))
+    print(slice_me(family, 1, -2))
+except Exception as error:
+    print(error)
