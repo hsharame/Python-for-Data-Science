c1c6ff034f9f1f11a97987a320ddafb7e34b5ca2
Hanna Sharameta Veulay <167891494+hsharame@users.noreply.github.com>
2026-01-29 10:33:55 +0000

    array(ex03): files added

---
+++ Array/ex03/zoom.py
@@ -0,0 +1,11 @@
+from load_image import ft_load
+
+def main():
+    """
+    Program loads the image "animal.jpeg", prints some information
+    about it and displays it after "zooming".
+    """
+    print(ft_load("animal.jpeg"))
+
+if __name__ == "__main__":
+    main()
