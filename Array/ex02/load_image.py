fb7198fde9fa816e44a967b887bec840b4c17c2e
Hanna Sharameta Veulay <167891494+hsharame@users.noreply.github.com>
2026-01-29 10:22:00 +0000

    array(ex02): done

--- Array/ex02/load_image.py
+++ Array/ex02/load_image.py
@@ -1,12 +1,23 @@
 from PIL import Image
 import numpy as np
+import os
 
 def ft_load(path: str) -> list:
     """
     Loads an image, prints its format, and its pixels
     content in RGB format.
     """
-    im = Image.open(path)
+    if not os.path.exists(path):
+        raise FileNotFoundError(f"File {path} does not exist")
+    if not os.path.isfile(path):
+        raise IsADirectoryError(f"{path} is directory")
+    if not path.lower().endswith((".jpg", ".jpeg")):
+        raise TypeError(f"{path}: unsupported format")
+    try:
+        im = Image.open(path)
+    except IOError:
+        raise AssertionError(f"Couldn't open {path}")
+
     im = im.convert("RGB")
     arr = np.array(im)
     print("The shape of image is:", arr.shape)
--- Array/ex02/tester.py
+++ Array/ex02/tester.py
@@ -1,3 +1,8 @@
 from load_image import ft_load
 
-print(ft_load("landscapye.jpg"))
+print(ft_load("landscape.jpg"))
+
+try:
+    print(ft_load("t.png"))
+except Exception as error:
+    print(error)
